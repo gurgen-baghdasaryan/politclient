054f4b1c4de1488edc63a839dc77e476ea772021
gugohh <oohhgug@gmail.com>
2022-03-14 00:21:31 +0100

    deploy

---
+++ src/App.js
@@ -0,0 +1,31 @@
+//RRD allows us to route our components
+import { Routes, Route } from "react-router-dom";
+
+// Importing our components
+import NavBar from "./components/NavBar/NavBar";
+import PoliticList from "./components/PoliticList/PoliticList";
+import CreatePolitic from "./components/CreatePolitic/CreatePolitic";
+import Politic from "./components/Politic/Politic";
+import Error from "./components/Error/Error";
+// import Error from "./components/Error/Error";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <div className="container p-4">
+        <Routes>
+          {/* Here we are going to include our routes */}
+          <Route exact path="/" element={<PoliticList />} />
+          <Route exact path="/CreatePolitic/" element={<CreatePolitic />} />
+          <Route exact path="/edit/:id" element={<CreatePolitic />} />
+          <Route exact path="/politic/:id" element={<Politic />} />
+          {/* This route is for error handling */}
+          <Route  path="*" element={<Error />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
